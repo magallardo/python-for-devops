d359ba9d5ef6043b7c981f444554aad19c5f7408
magallardo <gallardo@tibco.com>
2022-09-21 13:37:45 -0400

    Reformat

--- devopslib/randomfruit.py
+++ devopslib/randomfruit.py
@@ -5,14 +5,16 @@ def fruit():
     fruits = ["apple", "cherry", "strawberry"]
     return choices(fruits)[0]
 
+
 def meal(beverage):
-  my_fruit = fruit()
-  print(f"Your fruit is {my_fruit} and your beverage is {beverage}")
-  if my_fruit == "cherry":
-    complete_meal = f"Your meal is a {my_fruit} and {beverage}"
-    return complete_meal
-  alternate_meal = f"Your meal is a steak and {beverage}"
-  return alternate_meal
+    my_fruit = fruit()
+    print(f"Your fruit is {my_fruit} and your beverage is {beverage}")
+    if my_fruit == "cherry":
+        complete_meal = f"Your meal is a {my_fruit} and {beverage}"
+        return complete_meal
+    alternate_meal = f"Your meal is a steak and {beverage}"
+    return alternate_meal
+
 
 # var = 1
 # var = var
